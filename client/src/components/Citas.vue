28ceb1391b41cf6d416b7ad16dd7f51a82bea25b
carlos349 <carlos.gomes349@gmail.com>
2019-07-13 18:57:28 -0400

    Creacion de citas

--- client/src/components/Citas.vue
+++ client/src/components/Citas.vue
@@ -7,6 +7,8 @@
        :events="eventos"
        default-view="month"
        :disable-views="['years', 'year']"
+       events-on-month-view="short"
+       :on-event-click="onEventClick"
        :time-from="8 * 60"
        :time-to="18 * 60">
     </vue-cal>
@@ -21,35 +23,73 @@
 		        </button>
 		      </div>
 		      <div class="modal-body">
-		        <form v-on:submit.prevent="">
+		        <form v-on:submit.prevent="registroCita">
 							<div class="form-group">
 								<label for="nombre">Fecha</label>
-								<input type="date" class="form-control" name="nombreManicurista" placeholder="Nombre de la manicurista" >
+								<input type="date" v-model="fecha" class="form-control" name="nombreManicurista" placeholder="Nombre de la manicurista" >
                 <label for="nombre">Horario entrada</label>
-                <select class="form-control">
+                <select class="form-control" v-model="entrada">
+                  <option>Seleccione</option>
                   <option value="08:00">08:00 am</option>
                   <option value="09:00">09:00 am</option>
+                  <option value="10:00">10:00 am</option>
+                  <option value="11:00">11:00 am</option>
+                  <option value="12:00">12:00 am</option>
+                  <option value="13:00">13:00 pm</option>
+                  <option value="14:00">14:00 pm</option>
+                  <option value="15:00">15:00 pm</option>
                 </select>
                 <label for="nombre">Horario Salida</label>
-                <select class="form-control">
-                  <option value="08:00">08:00 am</option>
+                <select class="form-control" v-model="salida">
+                  <option>Seleccione</option>
                   <option value="09:00">09:00 am</option>
+                  <option value="10:00">10:00 am</option>
+                  <option value="11:00">11:00 am</option>
+                  <option value="12:00">12:00 pm</option>
+                  <option value="13:00">13:00 pm</option>
+                  <option value="14:00">14:00 pm</option>
+                  <option value="15:00">15:00 pm</option>
+                  <option value="16:00">16:00 pm</option>
                 </select>
 							</div>
               <div class="form-group">
 								<label for="identidad">Servicio</label>
-                <select class="form-control">
-                  <option value="08:00">08:00 am</option>
+                <select class="form-control" v-model="serv">
+                  <option>Seleccione</option>
+                  <option v-for="servicio of servicios" :value='servicio.nombre'>{{servicio.nombre}}</option>
                 </select>
 							</div>
 							<div class="form-group">
 								<label for="identidad">Manicurista</label>
-                <select class="form-control">
-                  <option value="08:00">08:00 am</option>
+                <select class="form-control" v-model="mani" >
+                  <option>Seleccione</option>
+                  <option v-for="manicurista of manicuristas" :value='manicurista.nombre'>{{manicurista.nombre}}</option>
                 </select>
 							</div>
 							<button class="btn btn-lg btn-info btn-block" type="submit">Registrar cita</button>
 		        </form>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+    <div class="modal fade" id="myModalTwo" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		  <div class="modal-dialog modal-dialog-centered" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header bg-info">
+		        <h5 class="modal-title text-white font-weight-bold" id="exampleModalCenterTitle">{{ selectedEvent.title }}</h5>
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true" class="text-white">&times;</span>
+		        </button>
+		      </div>
+		      <div class="modal-body">
+		        <p>Fecha: {{ selectedEvent.startDate }}</p>
+            <strong>Detalle de la cita:</strong><br><br>
+            <ul class="list-group">
+              <li class="list-group-item">Manicurista: {{ selectedEvent.content }}</li>
+              <li class="list-group-item">Hora de inicio: {{ selectedEvent.startTime }}</li>
+              <li class="list-group-item">Hora de finalización: {{ selectedEvent.endTime }}</li>
+            </ul><br>
+            <button type="button" class="btn btn-danger">Borrar cita</button>
 		      </div>
 		    </div>
 		  </div>
@@ -71,6 +111,22 @@
     }
   }
 
+  class Manicurista{
+  	constructor(nombre, documento, porcentaje, comision) {
+  		this.nombre = nombre;
+  		this.documento = documento;
+  		this.porcentaje = porcentaje;
+  		this.comision = comision;
+  	}
+  }
+
+  class Servicio{
+		constructor(nombre, precio) {
+			this.nombre = nombre;
+			this.precio = precio;
+		}
+	}
+
   export default  {
     components: {
       VueCal
@@ -81,22 +137,79 @@
         start:'',
         startHora:'',
         evento: new Event(),
-        eventos: []
+        eventos: [],
+        manicurista: new Manicurista(),
+  			manicuristas: [],
+        servicio: new Servicio(),
+ 			 	servicios: [],
+        mani: 'Seleccione',
+        serv:'Seleccione',
+        entrada: 'Seleccione',
+        salida:'Seleccione',
+        fecha: '',
+        selectedEvent: {}
       }
     },
     created(){
-      this.getCitas()
+      this.getCitas(),
+      this.getManicuristas(),
+      this.getServicios()
     },
     methods: {
+      onEventClick(event, e){
+        this.selectedEvent = event
+        $('#myModalTwo').modal('show')
+        e.stopPropagation()
+      },
       getCitas () {
         axios.get('citas')
         .then(res => {
           this.eventos = res.data
-
         })
       },
+      getManicuristas(){
+  			axios.get('manicuristas')
+  			.then(res => {
+  				this.manicuristas = res.data
+  			})
+  		},
+      getServicios() {
+				axios.get('servicios')
+				.then(res => {
+					this.servicios = res.data
+				})
+			},
       aja() {
         $('#myModal').modal('show')
+      },
+      registroCita(){
+        const horarioEntrada = this.fecha + " " + this.entrada
+        const horarioSalida = this.fecha + " " + this.salida
+        axios.post('citas', {
+          entrada: horarioEntrada,
+          salida: horarioSalida,
+          titulo: this.serv,
+          contenido: this.mani
+        })
+        .then(res => {
+          if(res.data.status == 'cita creada'){
+            this.$swal({
+						  type: 'success',
+						  title: 'Cita creada',
+						  showConfirmButton: false,
+						  timer: 1500
+						})
+						$('#myModal').modal('hide')
+						this.getCitas();
+          }else{
+            this.$swal({
+						  type: 'error',
+						  title: 'Cita ocupada',
+						  showConfirmButton: false,
+						  timer: 1500
+						})
+          }
+        })
       }
 
     }
@@ -116,11 +229,17 @@
   .vuecal:not(.vuecal--day-view) .vuecal__cell.selected {background-color: rgba(235, 255, 245, 0.4);}
   .vuecal__cell.selected:before {border-color: rgba(66, 185, 131, 0.5);}
   .calendario{
-    max-height: 80vh;
+    max-height: 90vh;
     margin-top:2%
   }
   .vuecal__event{
-    background-color:#4369E8
+    background-color:#4369E8;
+    opacity: 0.5;
+  }
+  .vuecal__event:hover{
+    background-color:#4369E8;
+    opacity: 1;
+    z-index:10
   }
   .citas{
     padding:10px;
--- routes/Citas.js
+++ routes/Citas.js
@@ -9,4 +9,34 @@ citas.get('/', async (req, res) => {
   res.json(citas)
 })
 
+citas.post('/', (req, res) => {
+  const dataCitas = {
+    start: req.body.entrada,
+    end: req.body.salida,
+    title: req.body.titulo,
+    content: req.body.contenido
+  }
+
+  Citas.findOne({
+    start: req.body.entrada,
+    content: req.body.contenido
+  })
+  .then(citas => {
+    if (!citas) {
+      Citas.create(dataCitas)
+      .then(citas => {
+        res.json({status: 'cita creada'})
+      })
+      .catch(err => {
+        res.send('error: ' + err)
+      })
+    }else{
+      res.json({status: 'cita ocupada'})
+    }
+  })
+  .catch(err => {
+    res.send('error: ' + err)
+  })
+})
+
 module.exports = citas
