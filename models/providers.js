04ec95f0972e354ed67a6f2ee6f19497f0578ae6
CristiansSans <cristiansleonardo@gmail.com>
2020-04-03 18:29:17 -0400

    Inventory backend advance

--- models/providers.js
+++ models/providers.js
@@ -5,6 +5,18 @@ const providerSchema = new Schema({
   nombre:{
     type:String
   },
+  rut:{
+    type:Number
+  },
+  contacto:{
+    type:String
+  },
+  contactoAdicional:{
+    type:String
+  },
+  ubicacion:{
+    type:String
+  },
 })
 
 module.exports = Provider = mongoose.model("provider", providerSchema)
--- routes/Inventario.js
+++ routes/Inventario.js
@@ -3,6 +3,7 @@ const inventory = express.Router()
 const cors = require('cors');
 const Inventory = require('../models/Inventario')
 const Provider = require('../models/providers')
+const History = require('../models/historyClosedInventory')
 inventory.use(cors())
 
 inventory.get('/', async (req, res) => {
@@ -12,6 +13,10 @@ inventory.get('/', async (req, res) => {
 inventory.get('/getProvider', async (req, res) => {
     const provider = await Provider.find()
     res.json(provider)
+})
+inventory.get('/getHistory', async (req, res) => {
+    const history = await History.find()
+    res.json(history)
 })
 inventory.put('/:id', (req, res) => {
     Inventory.findByIdAndUpdate(req.params.id, {
@@ -42,6 +47,50 @@ inventory.put('/add/:id', (req, res) => {
       .catch(err => {
         res.send('error: ' + err)
       })
+})
+inventory.post('/closeInventory', (req, res) => {
+    var array = req.body.array
+    var date = new Date()
+    History.findOne({
+        mes: date.getMonth()
+    })
+    .then(done => {
+        if (!done) {
+            for (let i = 0; i < array.length; i++) {
+                Inventory.findByIdAndUpdate(array[i].id, {
+                $set: {
+                    cantidad:array[i].count,
+                    entry:0
+                }
+                })
+                .then(aver => {})
+                var diferencia = parseFloat(array[i].count) - parseFloat(array[i].ideal)
+                if (diferencia == 0) {
+                    diferencia = "Sin diferencia"
+                }
+                if (diferencia > 0) {
+                    diferencia = "+" + diferencia + " Productos"
+                }
+                if (diferencia < 0) {
+                    diferencia = diferencia + " Productos"
+                }
+                const product = {
+                    medida:array[i].medida,
+                    totalIdeal:array[i].ideal,
+                    totalReal:array[i].count,
+                    diferencia:diferencia,
+                    mes:date.getMonth()
+                }
+                History.create(product)
+                .then(ready => {})   
+            }
+            res.json({status: 'ok'})
+        }
+        else{
+            res.json({status: 'exist'})
+        }
+    })
+    
 })
 inventory.post('/', (req, res) => {
     const product = {
@@ -77,7 +126,11 @@ inventory.post('/', (req, res) => {
 })
 inventory.post('/addProvider', (req, res) => {
     const product = {
-        nombre: req.body.name
+        nombre: req.body.name,
+        rut: req.body.rut,
+        contacto : req.body.contacto,
+        contactoAdicional: req.body.contactoPlus,
+        ubicacion: req.body.direccion
     }
     
     Provider.findOne({
