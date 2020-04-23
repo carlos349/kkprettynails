c8837d06db8214bbd4a9b261d5aef7847b884aac
carlos349 <carlos.gomes349@gmail.com>
2020-04-23 15:38:04 -0400

    fixed mail constructor

--- routes/Clients.js
+++ routes/Clients.js
@@ -41,7 +41,7 @@ clients.post('/sendmail', upload.array('image', 3),  async (req, res, next) => {
             html: `
             <div>
                 <center>
-                    <img style="width: 100%; heigth:auto; src="${imgMails}${array.image}" alt="Imagen de correo">
+                    <img style="width: 100%; heigth:auto;" src="${imgMails}${array.image}" alt="Imagen de correo">
                 </center> 
             </div>
             `
@@ -80,7 +80,7 @@ clients.post('/sendmail', upload.array('image', 3),  async (req, res, next) => {
             subject: array.subject,
             html: `<div style="width: 50%;margin: auto;background-color: #f8f9fe;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);padding: 20px;font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#32325d;">
                     <center>
-                       <img style="width: 50%; heigth:auto; src="${imgMails}${array.image}" alt="Imagen de correo"> 
+                       <img style="width: 50%; heigth:auto;" src="${imgMails}${array.image}" alt="Imagen de correo"> 
                     </center>
                     
                 ${array.text}
@@ -105,15 +105,15 @@ clients.post('/sendmail', upload.array('image', 3),  async (req, res, next) => {
             subject: array.subject,
             html: `<div style="width: 50%;margin: auto;background-color: #f8f9fe;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);padding: 20px;font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#32325d;">
                     <center>
-                       <img style="width: 50%; heigth:auto; src="${imgMails}${array.image}" alt="Imagen de correo"> 
+                       <img style="width: 50%; heigth:auto;" src="${imgMails}${array.image}" alt="Imagen de correo"> 
                     </center>
                     
                 ${array.text}
 
                 <center>
                     <div style="display: inline-block;">
-                        <img style="width: 50%; heigth:auto; src="${imgMails}${array.imageTwo}" alt="Imagen de correo2"> 
-                        <img style="width: 50%; heigth:auto; src="${imgMails}${array.imageThree}" alt="Imagen de correo3"> 
+                        <img style="width: 50%; heigth:auto;" src="${imgMails}${array.imageTwo}" alt="Imagen de correo2"> 
+                        <img style="width: 50%; heigth:auto;" src="${imgMails}${array.imageThree}" alt="Imagen de correo3"> 
                     </div>
                 </center>
                 ${array.textTwo}
@@ -136,7 +136,7 @@ clients.post('/sendmail', upload.array('image', 3),  async (req, res, next) => {
             html: `<div style="width: 50%;margin: auto;background-color: #f8f9fe;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);padding: 20px;font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#32325d;">   
                 ${array.text}
                 <center>
-                <img style="width: 50%; heigth:auto; src="${imgMails}${array.image}" alt="Imagen de correo"> 
+                <img style="width: 50%; heigth:auto;" src="${imgMails}${array.image}" alt="Imagen de correo"> 
                 </center>
                 <style>h1{color:#32325d;}h2{color:#32325d;}h3{color:#32325d;}h4{color:#32325d;}h5{color:#32325d;}</style>
             </div>`
@@ -161,11 +161,11 @@ clients.post('/sendmail', upload.array('image', 3),  async (req, res, next) => {
             subject: array.subject,
             html: `<div style="width: 50%;margin: auto;background-color: #f8f9fe;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);padding: 20px;font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#32325d;">     
                 <center>
-                    <img style="width: 50%; heigth:auto; src="${imgMails}${array.image}" alt="Imagen de correo"> 
+                    <img style="width: 50%; heigth:auto;" src="${imgMails}${array.image}" alt="Imagen de correo"> 
                 </center>
                     ${array.text}
                 <center>
-                    <img style="width: 50%; heigth:auto; src="${imgMails}${array.imageTwo}" alt="Imagen de correo2"> 
+                    <img style="width: 50%; heigth:auto;" src="${imgMails}${array.imageTwo}" alt="Imagen de correo2"> 
                 </center>
                     ${array.textTwo}
                 <style>h1{color:#32325d;}h2{color:#32325d;}h3{color:#32325d;}h4{color:#32325d;}h5{color:#32325d;}</style>
