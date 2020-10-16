9cebdaae8944817c2d676b9e5f4dd9960b587bca
carlos349 <carlos.gomes349@gmail.com>
2020-10-16 17:30:04 -0400

    send confirm date email to the client

--- routes/Citas.js
+++ routes/Citas.js
@@ -722,9 +722,76 @@ citas.get('/confirmDate/:id', (req, res) => {
               </div>
           `
         }
+
+        const mailTwo = {
+          from: "kkprettynails.syswa.net",
+          to: split[1],
+          subject: 'Cita confirmada',
+          html: `
+          <div style="width: 80%;max-width:1000px;margin:auto;padding:0;text-align:center;">
+                  <div style="width: 100%;height: 10vh;margin: auto;background-color: #181d81;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);padding: 10px;font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#181d81;text-align:justify;-webkit-box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);-moz-box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);">
+                      <div style="width: 80px;margin:auto;border-radius:55%;background-color:#f8f9fa;padding: 10px;">     
+                          <img style="width: 100%;" src="${imgMails}syswa-isotipo.png" alt="Logo kkprettynails">
+                      </div>
+                  </div>
+                  <div style="width: 100%;margin: auto;padding-top: 5%;font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#181d81;padding-bottom: 20px;padding-left:2px;">
+                      <center>
+                        <div style="width:100%;text-align: center;">
+                          <h1 style="text-align: center;color:#181d81;">Información </h1>
+                          <img style="height:80px;width:100px;margin-top:-20px;" src="${imgMails}logokk.png" alt="Logo kkprettynails">
+                          <p style="text-align:center;margin-top:10px;font-size:16px;"> <strong>Hola ${split[0]}, le informamos que se ha confirmado su cita con exito.</strong> <br> 
+                          Si por algún mptivo desea cambiar o cancelar su cita por favor comunicarnos a la brevedad para liberar la hora tomada. <br> Los detalles del agendamiento se encuentran espeficicados a continuación. <br>
+                          </p> <br>
+                          <p style="text-align:left;margin-top:10px;font-size:14px;font-weight: 300;"> 
+                              <strong> Profesional: </strong> ${lenders}. <br>
+                              <strong> Servicios:</strong> ${services}. <br>
+                              <strong> Horarios de entrada:</strong> ${start}. <br>
+                          </p>
+
+                          <p style="text-align:left;margin-top:10px;font-size:16px;"> 
+                            <img style="height:25px;width:25px;" src="${imgMails}calendar.png" alt="Logo kkprettynails"> 
+                            <b style="margin-top:-5px">${splitDate}</b> <br>
+                            <img style="height:25px;width:25px;" src="${imgMails}market.png" alt="Logo kkprettynails"><a style="text-align:center;font-size:16px;" href="https://goo.gl/maps/m5rVWDEiPj7q1Hxh9"><b style="font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#181d81;font-size:16px;margin-top:-5px"> Av. Pedro de Valdivia 3474 Caracol Ñuñoa, Local 53-B Ñuñoa, Chile. </b></a>   <br>
+                          </p>
+                          
+                        <div>
+                      </center>
+                  </div>
+                  <div style="width: 100%;background-color: #f0f1f3;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);margin: auto;font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#181d81;padding-bottom:8px;-webkit-box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);-moz-box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);">
+                    <center>
+                    <div style="width:100%;">
+                        <center>
+                        <p style="text-align:center;font-size:14px;"><strong> Contáctanos</strong></p>
+                          <a  href="mailto:kkprettynails@gmail.com" style="margin-left:40px;text-decoration:none;"> 
+                            <img style="width:4%;" src="${imgMails}mail.png" alt="Logo mail">
+                          </a>
+                          <a  href="https://www.instagram.com/kkprettynails/" style="margin-left:40px;text-decoration:none;">
+                            <img style="width:4%;" src="${imgMails}ig.png" alt="Logo ig">
+                          </a>
+                          <a  href="https://api.whatsapp.com/send?phone=56972628949&text=&source=&data=&app_absent=" style="margin-left:20px;text-decoration:none;">
+                            <img style="width:4%;" src="${imgMails}ws.png" alt="Logo ws">
+                          </a>
+                          <a  href="https://kkprettynails.cl" style="margin-left:40px;text-decoration:none;">
+                            <img style="width:4%;" src="${imgMails}web.png" alt="Logo web">
+                          </a>
+                        <br>
+                        <a style="text-align:center;font-size:14px;" href="https://goo.gl/maps/m5rVWDEiPj7q1Hxh9"> Av. Pedro de Valdivia 3474 Caracol Ñuñoa, Local 53-B Ñuñoa, Chile.</a>
+                        </center>
+                    </div>
+                    </center>
+                </div>
+              </div>
+          `
+        }
         try {
           KMails.sendMail(mail)
-          res.json({status: 'ok', data: date[0]})
+          try {
+            KMails.sendMail(mailTwo)
+            res.json({status: 'ok', data: date[0]})
+          }catch(err){
+            res.json({status: 'bad'})
+            res.send(err)
+          }
         }catch(err){
           res.json({status: 'bad'})
           res.send(err)
