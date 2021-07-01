0e2697c7c080e3e6165efb6adebd607b5bc6f95f
Cristians Sanchez <cristiansleonardo@gmail.com>
2021-07-01 12:46:14 -0400

    some changes

--- routes/Citas.js
+++ routes/Citas.js
@@ -644,7 +644,7 @@ citas.post('/getBlocksFirst', (req, res) => {
   dateNow.setDate(dateNow.getDate() + 1)
   const formatDateTwo = dateNow.getFullYear() +"-"+(dateNow.getMonth() + 1)+"-"+dateNow.getDate()
 
-  var minutes = ((hourLast - 9) * 60) + 30
+  var minutes = ((hourLast - 10) * 60) + 30
   const totalFor = minutes / 15
   var input, output
   minutes = 30
